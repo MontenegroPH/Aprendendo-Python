71100b3390bfa431c5990956028ee13fdaab833c
MontenegroPH <pedromontegro25@gmail.com>
2025-07-20 22:09:12 +0000

    primeiro teste

---
+++ aula 1.py
@@ -0,0 +1,22 @@
+print('ola mundo!')
+print(7+4)
+print ('7'+'4')
+7+4
+'7'+'4'
+print('ola',5)
+nome='guanabara'
+idade=24
+peso=74.8
+print(nome,peso,idade)
+print(nome, idade, peso)
+nome=input('qual e o seu nome?')
+nome=input('qual a sua idade')
+nome=input("qual o seu peso?")
+print(nome,idade,peso)
+
+
+
+
+
+
+
